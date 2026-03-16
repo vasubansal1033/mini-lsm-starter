f0fbbd3e5bea2c1d6f600e46e0ff7617170e8f39
Vasu Bansal <vasubansal1998@gmail.com>
2026-03-16 23:10:41 +0530

    test copy

--- mini-lsm-starter/src/mem_table.rs
+++ mini-lsm-starter/src/mem_table.rs
@@ -23,6 +23,7 @@ use std::sync::atomic::AtomicUsize;
 use anyhow::Result;
 use bytes::Bytes;
 use crossbeam_skiplist::SkipMap;
+use nom::AsBytes;
 use ouroboros::self_referencing;
 
 use crate::iterators::StorageIterator;
@@ -53,7 +54,12 @@ pub(crate) fn map_bound(bound: Bound<&[u8]>) -> Bound<Bytes> {
 impl MemTable {
     /// Create a new mem-table.
     pub fn create(_id: usize) -> Self {
-        unimplemented!()
+        MemTable {
+            map: Arc::new(SkipMap::new()),
+            wal: None,
+            id: _id,
+            approximate_size: Arc::new(AtomicUsize::new(0)),
+        }
     }
 
     /// Create a new mem-table with WAL
@@ -87,7 +93,12 @@ impl MemTable {
 
     /// Get a value by key.
     pub fn get(&self, _key: &[u8]) -> Option<Bytes> {
-        unimplemented!()
+        let value = self.map.get(&Bytes::copy_from_slice(_key));
+
+        match value {
+            Some(entry) => Some(entry.value().clone()),
+            None => None,
+        }
     }
 
     /// Put a key-value pair into the mem-table.
@@ -96,7 +107,13 @@ impl MemTable {
     /// In week 2, day 6, also flush the data to WAL.
     /// In week 3, day 5, modify the function to use the batch API.
     pub fn put(&self, _key: &[u8], _value: &[u8]) -> Result<()> {
-        unimplemented!()
+        let estimated_size = _key.len() + _value.len();
+
+        let entry = self
+            .map
+            .insert(Bytes::copy_from_slice(_key), Bytes::copy_from_slice(_value));
+
+        Ok(())
     }
 
     /// Implement this in week 3, day 5; if you want to implement this earlier, use `&[u8]` as the key type.
